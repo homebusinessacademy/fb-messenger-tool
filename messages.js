94cce1ba06a33d3b9c2eb7a043c54ef1f2acd9ca
homebusinessacademy <admin@thehomebusinessacademy.com>
2026-02-26 10:24:41 -0700

    update: new message variations from Paul

--- messages.js
+++ messages.js
@@ -6,10 +6,10 @@
 
 export const MESSAGE_VARIATIONS = [
   // Variation 1
-  "Hey {{first_name}}, hope you're having a {great|wonderful|fantastic} day! Quick question. I recently {ran across|came across|found} a {project|business project|business model} that looks like it could be pretty {lucrative|profitable|great}. Would you be open to {taking a peek|checking it out|taking a look}? No worries {if not|if no}, just let me know.",
+  "Hey {{first_name}}, hope you're having a {great|wonderful|fantastic} day! Quick question. I recently {ran across|came across|found} a {project|business project|business model} that looks like it could be pretty {lucrative|profitable}. Would you be open to {taking a peek|checking it out|taking a look}? No worries {if not|if it's not for you}, just let me know.",
 
   // Variation 2
-  "Hey {{first_name}}, hope you're doing well. This might not be for you, but you came to mind when I saw it, so {wanted to touch base|thought I'd reach out} just in case. It's an online {marketing|business} project, different from anything I've seen before, and looks like it could be a pretty {good money maker|income stream|great income generator}. Does that sound like something you'd be open to {taking a look at|checking out}?",
+  "Hey {{first_name}}, hope you're doing well. This might not be for you, but you came to mind when I saw it, so {wanted to touch base|thought I'd reach out} just in case. It's an online {marketing|business} project, different from anything I've seen before, and looks like it could be a pretty {solid money maker|good income stream|great cash flow generator}. Does that sound like something you'd be open to {taking a look at|checking out}?",
 
   // Variation 3
   "Hi {{first_name}}, hope {all is well in your world|everything's going great|life is treating you well}. 🙂 I just {found|came across} something that made me think of you. It's an online business that's pretty {unique|different|one of a kind}. Honestly, I've never seen anything quite like it. Anyway, it looks like it could have some pretty {good|solid|great} potential so I wanted to reach out to see if you'd be open to taking a look?",
